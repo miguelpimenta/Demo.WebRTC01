b11657382c0fd49e571ea3da93dd9b646e084785
MiguelPimenta <miguel.pimenta@outlook.com>
2022-09-08 00:00:51 +0100

    ....

--- Server/server.js
+++ Server/server.js
@@ -2,6 +2,7 @@ const http = require("http");
 const server = require("websocket").server;
 
 const httpServer = http.createServer(() => {});
+
 httpServer.listen(1337, () => {
   console.log("Server listening at port 1337");
 });
